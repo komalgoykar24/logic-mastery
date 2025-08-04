6ffe0289f6a715ce9769544dbc4c33148f10c99a
komalgoykar24 <komalgoykar2002@gmail.com>
2025-08-04 16:05:50 +0530

    NVERTED HALF-PYRAMID with 1 2 3 4 5 1 2 3 4 1 2 3 1 2 1

---
+++ src/NVERTEDHALFPYRAMIDwithNumbers.java
@@ -0,0 +1,16 @@
+public class NVERTEDHALFPYRAMIDwithNumbers {
+    public static void find(){
+        int k=5;
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=k;j++)
+            {
+                System.out.print(j+" ");
+            }
+            k--;
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+    find();
+    }
+}
