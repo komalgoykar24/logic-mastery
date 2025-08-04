ec8173f07357769229ca74ea16a0d86b7db4a9f8
komalgoykar24 <komalgoykar2002@gmail.com>
2025-08-04 16:48:11 +0530

    Dimand Pattern

---
+++ src/DiamondPattern.java
@@ -0,0 +1,31 @@
+public class DiamondPattern {
+    public static void main(String[] args) {
+        int n = 5;
+
+        // Upper Half
+        for (int i = 1; i <= n; i++) {
+            // Print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Lower Half
+        for (int i = n - 1; i >= 1; i--) {
+            // Print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
