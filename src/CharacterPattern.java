9f64fd90c1e7121e7ea4303c0a18a5d48404a67d
komalgoykar24 <komalgoykar2002@gmail.com>
2025-08-04 15:25:35 +0530

    Chararter Pattern A B C D E F G H I J K L M N O P Q R S T U

---
+++ src/CharacterPattern.java
@@ -0,0 +1,16 @@
+public class CharacterPattern {
+    public static void find(){
+        char ch='A';
+        for(int i=1;i<=6;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(ch+" ");
+                ch++;
+            }
+            System.out.println();
+        }
+
+    }
+    public static void main(String[] args) {
+     find();
+    }
+}
