b14ccf7315fd785fa4399fc9ed675f012f1e2917
komalgoykar24 <komalgoykar2002@gmail.com>
2025-08-04 15:12:04 +0530

    NumberPattern Code

---
+++ src/NumberPattern.java
@@ -0,0 +1,14 @@
+public class NumberPattern {
+    public static void Pattern() {
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+
+    }
+    public static void main(String[] args) {
+     Pattern();
+    }
+}
